de02e29a177ee52ba9c4af78adb9c079c2b21db8
OcculxRift <olzhasqr@gmail.com>
2025-05-02 11:17:22 +0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import { join } from 'path'
+
+// Функция для копирования дополнительных файлов
+function copyFiles() {
+  const files = ['_redirects', '404.html']
+  files.forEach(file => {
+    copyFileSync(
+      join(__dirname, file),
+      join(__dirname, 'docs', file)
+  })
+}
 
 export default defineConfig({
   base: '/order-tracker/',
   plugins: [react()],
   build: {
     outDir: 'docs',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      plugins: [{
+        name: 'copy-files',
+        closeBundle: copyFiles
+      }]
+    }
   }
 })
