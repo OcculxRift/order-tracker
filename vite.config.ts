3be6dd785fea3003f94e9305da541d27ce68ae37
OcculxRift <olzhasqr@gmail.com>
2025-05-02 10:57:22 +0500

    Commit changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/order-tracker/',
+  plugins: [react()], // Теперь импорт используется
   build: {
     outDir: 'docs'
   }
